086b92b39116efc6c0c7feceef5f5b0538093fde
NishithKody <113385563+NishithKody@users.noreply.github.com>
2023-06-06 16:48:04 +0530

    Create Max_sum_subarray.js

---
+++ JavaScript/Max_sum_subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+
+    let res = nums[0]
+    let sum = 0
+
+    for(let i=0; i<nums.length; i++){
+          sum =sum + nums[i]
+          res = Math.max(sum,res)
+          if(sum<0)
+            sum=0
+    }
+
+    return res
+    
+};
